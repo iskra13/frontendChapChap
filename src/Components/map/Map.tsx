0ac4bc508c07e4ec901046953b58af2565a95368
Ксения Капурина <kseniakap096@gmail.com>
2024-12-08 21:32:46 +0300

    Добавлено ограчение по зуму карты

--- src/Components/map/Map.tsx
+++ src/Components/map/Map.tsx
@@ -13,7 +13,12 @@ const generalPolygon: any = getGeneralPolygon();
 const Map = () => {
   return (
     <div className={styles.map} style={{ height: '100vh' }}>
-      <MapContainer center={startPosition} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <MapContainer
+        center={startPosition}
+        zoom={14}
+        minZoom={14}
+        style={{ height: '100%', width: '100%' }}
+      >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Polygon pathOptions={{ color: '#223623' }} positions={generalPolygon} />
       </MapContainer>
--- src/Components/map/utils/GetStartPositionCamera.ts
+++ src/Components/map/utils/GetStartPositionCamera.ts
@@ -1,1 +1,1 @@
-export const getStartPositionCamera = () => [56.81917599928795, 37.30446963387013];
+export const getStartPositionCamera = () => [56.811917599928795, 37.30446963387013];
